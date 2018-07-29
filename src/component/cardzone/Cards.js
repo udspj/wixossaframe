9ca2f7846cf4c9275470322e2618dd1e9b1bcb10
sunpeijia <1311107143@qq.com>
2018-07-29 22:30:54 +0800

    no message

--- src/component/cardzone/Cards.js
+++ src/component/cardzone/Cards.js
@@ -6,6 +6,7 @@ import {Entity} from 'aframe-react';
 import React from 'react';
 // import ReactDOM from 'react-dom';
 import slot from '../../resource/slotcard.png';
+import cardbk from '../../resource/cardbk_b.png';
 
 class Cards extends React.Component {
   constructor(props) {
@@ -24,6 +25,19 @@ class Cards extends React.Component {
         scale='0.22 0.3 1'
         rotation='90 0 0' 
         material={{src: slot, side:'double', transparent:true}}>
+        </Entity>
+
+        <Entity geometry={{primitive: 'box'}} 
+        position={'1.1 '+(this.props.cardY+0.05)+' '+(this.props.deskZ-0.5)} 
+        scale='0.2 0.1 0.28'
+        material={{color: '#333'}}>
+        </Entity>
+
+        <Entity geometry={{primitive: 'plane'}} 
+        position={'1.1 '+(this.props.cardY+0.1)+' '+(this.props.deskZ-0.5)} 
+        scale='0.2 0.28 1'
+        rotation='90 180 0' 
+        material={{src: cardbk, side:'double', transparent:true}}>
         </Entity>
 
       </Entity>
