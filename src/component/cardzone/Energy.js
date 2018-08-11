7a7f455b5eb6668a43f5d0dad0e013d5d0542783
sunpeijia <1311107143@qq.com>
2018-08-11 22:28:55 +0800

    no message

--- src/component/cardzone/Energy.js
+++ src/component/cardzone/Energy.js
@@ -7,24 +7,41 @@ import React from 'react';
 // import ReactDOM from 'react-dom';
 import slot from '../../resource/slotenergy.png';
 
-class Energy extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+import store from '../../store/store';
+import { mainCardsList } from '../utils/maincards';
 
-  componentDidMount() {
+class Energy extends React.Component {
+
+  handleClick(cardinfo) {
+    console.log(cardinfo)
   }
 
   render () {
+    const cardy = this.props.cardY;
+    const cardz = this.props.deskZ;
+    const list = mainCardsList;
     return (
       <Entity>
 
         <Entity geometry={{primitive: 'plane'}} 
-        position={'-1.1 '+this.props.cardY+' '+(this.props.deskZ-0.1)} 
+        position={'-1.1 '+cardy+' '+(cardz-0.1)} 
         scale='0.3 1.2 1'
         rotation='90 0 0' 
         material={{src: slot, side:'double', transparent:true}}>
         </Entity>
+
+        {store.getState().deck.energy.map(function(result, reactid) {
+          return (
+            <Entity key={'deckhand'+reactid} 
+            geometry={{primitive: 'plane'}} 
+            position={'-1.1 '+(cardy+0.001+0.001*reactid)+' '+(cardz-0.58+0.12*reactid)} 
+            scale='0.2 0.28 1'
+            rotation='90 -90 0' 
+            material={{src: require('../../resource/'+list[result]['img']+'.png'), side:'double', transparent:true}}
+            events={{click: () => this.handleClick(list[result])}}>
+            </Entity>
+          )
+        }.bind(this))}
 
       </Entity>
     );
